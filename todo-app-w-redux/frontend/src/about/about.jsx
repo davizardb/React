6e1989b2201e6fba71443379c35377f920ff783a
davizardb <davi.luis@aluno.uece.br>
2019-07-02 18:05:40 -0300

    todo-app com redux

---
+++ todo-app-w-redux/frontend/src/about/about.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import PageHeader from '../template/pageHeader'
+
+export default props => (
+    <div>
+        <PageHeader name='Sobre' small='Meu cacetão'></PageHeader>
+        <h2>Nossa História</h2>
+        <p>Lorem ipsum dolor ist amet...</p>
+        <h2>Missão e Valor</h2>
+        <p>Lorem ipsum dolor sit amet...</p>
+        <h2>Imprensa</h2>
+        <p>Lorem ipsum dolor sit amet...</p>
+    </div> 
+)
