9a896cb7c2d7d78359a1eb247c844501e2bcf667
davizardb <davi.lui@aluno.uece.br>
2020-04-15 17:57:27 -0300

    commit 2020 for backup

---
+++ Fail/failed/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <div className="App">
+        Fail wall
+      </div>
+      <div className="red">
+       <p className="pop">
+         ./src/index.js
+         <br/>Nesse mural ponha suas falhas mais significativas. 
+         <br/>Somos um clã únidos por um propósito e você é fundamental para nós. 
+       </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
